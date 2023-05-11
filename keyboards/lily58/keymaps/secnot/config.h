c91aa9fe55f00c9d556b5c6dcb3e0ccd0d97afa5
secnot <secnot@secnot.com>
2023-05-11 18:30:16 +0200

    config: Increase debouncing time

--- keyboards/lily58/keymaps/secnot/config.h
+++ keyboards/lily58/keymaps/secnot/config.h
@@ -36,6 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
+#define DEBOUNCE 10
 
 // Underglow
 /*
